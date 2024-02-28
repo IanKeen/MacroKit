8e8a01c7d6a012285ec61edfe74e4ea0cd00dee8
Ian Keen <iankeen82@gmail.com>
2024-02-28 13:13:40 -0800

    im sure this is somehow apples fault

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     ],
     dependencies: [
         // Depend on the latest Swift 5.9 prerelease of SwiftSyntax
-        .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0-swift-5.9-DEVELOPMENT-SNAPSHOT-2023-04-25-b"),
+        .package(url: "https://github.com/apple/swift-syntax.git", exact: "509.0.0-swift-DEVELOPMENT-SNAPSHOT-2023-06-05-a"),
     ],
     targets: [
         // Macro implementation that performs the source transformation of a macro.
